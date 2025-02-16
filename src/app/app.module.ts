ada5580e4debd2e9add70391c436a731c54eef62
xlnc1337 <fbahader@gmail.com>
2025-02-16 20:59:59 +0100

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { NgModule, APP_INITIALIZER } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ClipboardModule } from 'ngx-clipboard';
-import { TranslateModule } from '@ngx-translate/core';
-import { InlineSVGModule } from 'ng-inline-svg-2';
-import {NgbModule, NgbPagination, NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CommonModule } from '@angular/common';
-import { AuthService } from './modules/auth/services/auth.service';
-import { environment } from 'src/environments/environment';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {ClipboardModule} from 'ngx-clipboard';
+import {TranslateModule} from '@ngx-translate/core';
+import {InlineSVGModule} from 'ng-inline-svg-2';
+import {NgbModule, NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CommonModule} from '@angular/common';
+import {AuthService} from './modules/auth';
+import {environment} from 'src/environments/environment';
+import {SweetAlert2Module} from '@sweetalert2/ngx-sweetalert2';
 // #fake-start#
-import { FakeAPIService } from './_fake/fake-api.service';
+import {FakeAPIService} from './_fake';
 import {AuthInterceptor} from "./modules/auth/auth.interceptor";
 import {FormsModule} from "@angular/forms";
+
 // #fake-end#
 
 function appInitializer(authService: AuthService) {
