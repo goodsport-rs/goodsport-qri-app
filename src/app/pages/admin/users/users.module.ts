6a8a1f8f71a2910f2ab846a59847663a28bb71db
xlnc1337 <fbahader@gmail.com>
2025-02-17 22:17:35 +0100

    fixed ui

--- src/app/pages/admin/users/users.module.ts
+++ src/app/pages/admin/users/users.module.ts
@@ -5,24 +5,26 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UsersComponent } from './users.component';
 import { UserDetailsComponent } from './details/details.component';
+import {SharedModule} from "../../../_metronic/shared/shared.module";
 
 @NgModule({
   declarations: [UsersComponent, UserDetailsComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: UsersComponent,
-      },
-      {
-        path: 'details/:id',
-        component: UserDetailsComponent,
-      },
-    ]),
-    FormsModule,
-    NgbModule,
-    ReactiveFormsModule,
-  ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: UsersComponent,
+            },
+            {
+                path: 'details/:id',
+                component: UserDetailsComponent,
+            },
+        ]),
+        FormsModule,
+        NgbModule,
+        ReactiveFormsModule,
+        SharedModule,
+    ],
 })
 export class UsersModule {}
